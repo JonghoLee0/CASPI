51bc9c9334fb10cce52b764139f65c684b1aa747
Jongho Lee <39136115+JonghoLee0@users.noreply.github.com>
2023-03-27 13:55:52 -0500

    Delete compute_photon_prob.m

--- compute_photon_prob.m
+++
@@ -1,6 +0,0 @@
-function photon_prob = compute_photon_prob(avg_photon_vect)
-
-sumcum = cumsum(avg_photon_vect);
-sumcum = [0; sumcum(1:end-1)];
-
-photon_prob = (1 - exp(-avg_photon_vect)).*exp(-sumcum);
